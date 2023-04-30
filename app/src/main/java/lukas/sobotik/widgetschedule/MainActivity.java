b63c93a40db1aa8ecd87a36ba9bc14902bf624d2
Pucky <osmset@gmail.com>
2023-04-30 15:15:06 +0200

    Fragment Switching

--- app/src/main/java/lukas/sobotik/widgetschedule/MainActivity.java
+++ app/src/main/java/lukas/sobotik/widgetschedule/MainActivity.java
@@ -7,34 +7,18 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.materialswitch.MaterialSwitch;
-import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.textfield.TextInputLayout;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextInputLayout scheduleURL;
-    Button saveButton;
-    LinearLayout containsWeekDayLayout, removeEmptyItemsLayout, hideLastTableLayout;
-    MaterialSwitch containsWeekDaySwitch, removeEmptyItemsSwitch, hideLastTableSwitch;
     BottomNavigationView bottomNavigationView;
-
-    SettingsDatabaseHelper settingsDatabaseHelper;
-    ScheduleDatabaseHelper scheduleDatabaseHelper;
-    List<SettingsEntry> settingsList;
-    List<ScheduleEntry> scheduleList;
-    List<String> HTMLTables;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,129 +26,32 @@ public class MainActivity extends AppCompatActivity {
 
         initialize();
 
-        loadDataFromDatabase();
+        SettingsFragment settingsFragment = new SettingsFragment();
+        ScheduleFragment scheduleFragment = new ScheduleFragment();
 
-        saveButton.setOnClickListener(v -> {
-            String scheduleLink = Objects.requireNonNull(scheduleURL.getEditText()).getText().toString().toLowerCase().trim();
-
-            Log.d("Custom Logging", String.valueOf(containsWeekDaySwitch.isChecked()));
-            Log.d("Custom Logging", String.valueOf(removeEmptyItemsSwitch.isChecked()));
-            Log.d("Custom Logging", String.valueOf(hideLastTableSwitch.isChecked()));
-
-            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().ScheduleURL, scheduleLink));
-            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().ContainsDayOfWeek, String.valueOf(containsWeekDaySwitch.isChecked())));
-            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().RemoveEmptyItems, String.valueOf(removeEmptyItemsSwitch.isChecked())));
-            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().HideLastTable, String.valueOf(hideLastTableSwitch.isChecked())));
-            fetchDataFromURL(scheduleLink);
-        });
-
-        containsWeekDayLayout.setOnClickListener(view -> {
-            containsWeekDaySwitch.setChecked(!containsWeekDaySwitch.isChecked());
-            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().ContainsDayOfWeek, String.valueOf(containsWeekDaySwitch.isChecked())));
-        });
-        removeEmptyItemsLayout.setOnClickListener(view -> {
-            removeEmptyItemsSwitch.setChecked(!removeEmptyItemsSwitch.isChecked());
-            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().RemoveEmptyItems, String.valueOf(removeEmptyItemsSwitch.isChecked())));
-        });
-        hideLastTableLayout.setOnClickListener(view -> {
-            hideLastTableSwitch.setChecked(!hideLastTableSwitch.isChecked());
-            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().HideLastTable, String.valueOf(hideLastTableSwitch.isChecked())));
-        });
+        // Add the fragment to the container view with a FragmentTransaction
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.schedule_fragment_container, settingsFragment);
+        transaction.commit();
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
             if (item.getItemId() == R.id.bottom_navigation_settings) {
                 Log.d("Custom Logging", "Settings");
+                fragmentTransaction.replace(R.id.schedule_fragment_container, settingsFragment);
+                fragmentTransaction.commit();
                 return true;
             } else if (item.getItemId() == R.id.bottom_navigation_schedule) {
                 Log.d("Custom Logging", "Schedule");
+                fragmentTransaction.replace(R.id.schedule_fragment_container, scheduleFragment);
+                fragmentTransaction.commit();
                 return true;
             }
             return false;
         });
     }
 
-    private void loadDataFromDatabase() {
-        Cursor settingsCursor = settingsDatabaseHelper.readAllData();
-        if (settingsCursor.getCount() == 0) {
-            return;
-        }
-
-        while (settingsCursor.moveToNext()) {
-            int settings = -1;
-            if (Objects.equals(settingsCursor.getString(1), String.valueOf(new Settings().ScheduleURL))) {
-                settings = new Settings().ScheduleURL;
-            } else if (Objects.equals(settingsCursor.getString(1), String.valueOf(new Settings().ContainsDayOfWeek))) {
-                settings = new Settings().ContainsDayOfWeek;
-            } else if (Objects.equals(settingsCursor.getString(1), String.valueOf(new Settings().RemoveEmptyItems))) {
-                settings = new Settings().RemoveEmptyItems;
-            } else if (Objects.equals(settingsCursor.getString(1), String.valueOf(new Settings().HideLastTable))) {
-                settings = new Settings().HideLastTable;
-            }
-
-            settingsList.add(new SettingsEntry(settings, settingsCursor.getString(2), Integer.parseInt(settingsCursor.getString(0))));
-        }
-
-        for (SettingsEntry entry : settingsList) {
-            if (String.valueOf(entry.getSettingName()).equals(String.valueOf(new Settings().ScheduleURL))) {
-                Objects.requireNonNull(scheduleURL.getEditText()).setText(entry.getValue());
-            } else if (String.valueOf(entry.getSettingName()).equals(String.valueOf(new Settings().ContainsDayOfWeek))) {
-                boolean isChecked = entry.getValue().equals("true");
-                containsWeekDaySwitch.setChecked(isChecked);
-            } else if (String.valueOf(entry.getSettingName()).equals(String.valueOf(new Settings().RemoveEmptyItems))) {
-                boolean isChecked = entry.getValue().equals("true");
-                removeEmptyItemsSwitch.setChecked(isChecked);
-            } else if (String.valueOf(entry.getSettingName()).equals(String.valueOf(new Settings().HideLastTable))) {
-                boolean isChecked = entry.getValue().equals("true");
-                hideLastTableSwitch.setChecked(isChecked);
-            }
-        }
-
-        Cursor scheduleCursor = scheduleDatabaseHelper.readAllData();
-        if (scheduleCursor.getCount() == 0) {
-            return;
-        }
-
-        while (scheduleCursor.moveToNext()) {
-            scheduleList.add(new ScheduleEntry(Integer.parseInt(scheduleCursor.getString(0)), scheduleCursor.getString(1), scheduleCursor.getString(2)));
-
-            TextView textView = findViewById(R.id.schedule_html);
-            textView.setText(scheduleCursor.getString(2));
-        }
-    }
-
-    public void fetchDataFromURL(String url) {
-        Thread thread = new Thread(() -> {
-            try  {
-                Log.d("Custom Logging", "Fetching Data...");
-                try {
-                    Document doc = Jsoup.connect(url).get();
-                    scheduleDatabaseHelper.addItem(new ScheduleEntry(url, doc.html()));
-                } catch (IOException e) {
-                    Log.d("Custom Logging", "error " + e.getMessage());
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-        thread.start();
-    }
-
     public void initialize() {
-        settingsDatabaseHelper = new SettingsDatabaseHelper(this);
-        scheduleDatabaseHelper = new ScheduleDatabaseHelper(this);
-        settingsList = new ArrayList<>();
-        scheduleList = new ArrayList<>();
-        HTMLTables = new ArrayList<>();
-
-        scheduleURL = findViewById(R.id.schedule_url_text_input);
-        saveButton = findViewById(R.id.database_save_button);
-        containsWeekDayLayout = findViewById(R.id.contains_day_of_week_layout);
-        containsWeekDaySwitch = findViewById(R.id.contains_day_of_week_switch);
-        removeEmptyItemsLayout = findViewById(R.id.remove_empty_items_layout);
-        removeEmptyItemsSwitch = findViewById(R.id.remove_empty_items_switch);
-        hideLastTableLayout = findViewById(R.id.hide_last_table_layout);
-        hideLastTableSwitch = findViewById(R.id.hide_last_table_switch);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
     }
 }
---
+++ app/src/main/java/lukas/sobotik/widgetschedule/SettingsFragment.java
@@ -0,0 +1,197 @@
+package lukas.sobotik.widgetschedule;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.materialswitch.MaterialSwitch;
+import com.google.android.material.textfield.TextInputLayout;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SettingsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SettingsFragment extends Fragment {
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    TextInputLayout scheduleURL;
+    Button saveButton;
+    LinearLayout containsWeekDayLayout, removeEmptyItemsLayout, hideLastTableLayout;
+    MaterialSwitch containsWeekDaySwitch, removeEmptyItemsSwitch, hideLastTableSwitch;
+    SettingsDatabaseHelper settingsDatabaseHelper;
+    ScheduleDatabaseHelper scheduleDatabaseHelper;
+    List<SettingsEntry> settingsList;
+    List<ScheduleEntry> scheduleList;
+    List<String> HTMLTables;
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SettingsFragment.
+     */
+    public static SettingsFragment newInstance(String param1, String param2) {
+        SettingsFragment fragment = new SettingsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View inflatedView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        settingsDatabaseHelper = new SettingsDatabaseHelper(getContext());
+        scheduleDatabaseHelper = new ScheduleDatabaseHelper(getContext());
+        settingsList = new ArrayList<>();
+        scheduleList = new ArrayList<>();
+        HTMLTables = new ArrayList<>();
+
+        scheduleURL = inflatedView.findViewById(R.id.schedule_url_text_input);
+        saveButton = inflatedView.findViewById(R.id.database_save_button);
+        containsWeekDayLayout = inflatedView.findViewById(R.id.contains_day_of_week_layout);
+        containsWeekDaySwitch = inflatedView.findViewById(R.id.contains_day_of_week_switch);
+        removeEmptyItemsLayout = inflatedView.findViewById(R.id.remove_empty_items_layout);
+        removeEmptyItemsSwitch = inflatedView.findViewById(R.id.remove_empty_items_switch);
+        hideLastTableLayout = inflatedView.findViewById(R.id.hide_last_table_layout);
+        hideLastTableSwitch = inflatedView.findViewById(R.id.hide_last_table_switch);
+
+        loadDataFromDatabase(inflatedView);
+
+        saveButton.setOnClickListener(v -> {
+            String scheduleLink = Objects.requireNonNull(scheduleURL.getEditText()).getText().toString().toLowerCase().trim();
+
+            Log.d("Custom Logging", String.valueOf(containsWeekDaySwitch.isChecked()));
+            Log.d("Custom Logging", String.valueOf(removeEmptyItemsSwitch.isChecked()));
+            Log.d("Custom Logging", String.valueOf(hideLastTableSwitch.isChecked()));
+
+            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().ScheduleURL, scheduleLink));
+            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().ContainsDayOfWeek, String.valueOf(containsWeekDaySwitch.isChecked())));
+            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().RemoveEmptyItems, String.valueOf(removeEmptyItemsSwitch.isChecked())));
+            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().HideLastTable, String.valueOf(hideLastTableSwitch.isChecked())));
+            fetchDataFromURL(scheduleLink);
+        });
+
+        containsWeekDayLayout.setOnClickListener(view -> {
+            containsWeekDaySwitch.setChecked(!containsWeekDaySwitch.isChecked());
+            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().ContainsDayOfWeek, String.valueOf(containsWeekDaySwitch.isChecked())));
+        });
+        removeEmptyItemsLayout.setOnClickListener(view -> {
+            removeEmptyItemsSwitch.setChecked(!removeEmptyItemsSwitch.isChecked());
+            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().RemoveEmptyItems, String.valueOf(removeEmptyItemsSwitch.isChecked())));
+        });
+        hideLastTableLayout.setOnClickListener(view -> {
+            hideLastTableSwitch.setChecked(!hideLastTableSwitch.isChecked());
+            settingsDatabaseHelper.addItem(new SettingsEntry(new Settings().HideLastTable, String.valueOf(hideLastTableSwitch.isChecked())));
+        });
+
+        return inflatedView;
+    }
+
+    private void loadDataFromDatabase(View inflatedView) {
+        Cursor settingsCursor = settingsDatabaseHelper.readAllData();
+        if (settingsCursor.getCount() == 0) {
+            return;
+        }
+
+        while (settingsCursor.moveToNext()) {
+            int settings = -1;
+            if (Objects.equals(settingsCursor.getString(1), String.valueOf(new Settings().ScheduleURL))) {
+                settings = new Settings().ScheduleURL;
+            } else if (Objects.equals(settingsCursor.getString(1), String.valueOf(new Settings().ContainsDayOfWeek))) {
+                settings = new Settings().ContainsDayOfWeek;
+            } else if (Objects.equals(settingsCursor.getString(1), String.valueOf(new Settings().RemoveEmptyItems))) {
+                settings = new Settings().RemoveEmptyItems;
+            } else if (Objects.equals(settingsCursor.getString(1), String.valueOf(new Settings().HideLastTable))) {
+                settings = new Settings().HideLastTable;
+            }
+
+            settingsList.add(new SettingsEntry(settings, settingsCursor.getString(2), Integer.parseInt(settingsCursor.getString(0))));
+        }
+
+        for (SettingsEntry entry : settingsList) {
+            if (String.valueOf(entry.getSettingName()).equals(String.valueOf(new Settings().ScheduleURL))) {
+                Objects.requireNonNull(scheduleURL.getEditText()).setText(entry.getValue());
+            } else if (String.valueOf(entry.getSettingName()).equals(String.valueOf(new Settings().ContainsDayOfWeek))) {
+                boolean isChecked = entry.getValue().equals("true");
+                containsWeekDaySwitch.setChecked(isChecked);
+            } else if (String.valueOf(entry.getSettingName()).equals(String.valueOf(new Settings().RemoveEmptyItems))) {
+                boolean isChecked = entry.getValue().equals("true");
+                removeEmptyItemsSwitch.setChecked(isChecked);
+            } else if (String.valueOf(entry.getSettingName()).equals(String.valueOf(new Settings().HideLastTable))) {
+                boolean isChecked = entry.getValue().equals("true");
+                hideLastTableSwitch.setChecked(isChecked);
+            }
+        }
+
+        Cursor scheduleCursor = scheduleDatabaseHelper.readAllData();
+        if (scheduleCursor.getCount() == 0) {
+            return;
+        }
+
+        while (scheduleCursor.moveToNext()) {
+            scheduleList.add(new ScheduleEntry(Integer.parseInt(scheduleCursor.getString(0)), scheduleCursor.getString(1), scheduleCursor.getString(2)));
+
+            TextView textView = inflatedView.findViewById(R.id.schedule_html);
+            textView.setText(scheduleCursor.getString(2));
+        }
+    }
+
+    public void fetchDataFromURL(String url) {
+        Thread thread = new Thread(() -> {
+            try  {
+                Log.d("Custom Logging", "Fetching Data...");
+                try {
+                    Document doc = Jsoup.connect(url).get();
+                    scheduleDatabaseHelper.addItem(new ScheduleEntry(url, doc.html()));
+                } catch (IOException e) {
+                    Log.d("Custom Logging", "error " + e.getMessage());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread.start();
+    }
+}
